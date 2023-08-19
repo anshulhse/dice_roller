cd37be56b6038cb73b943eeb456e60668a917ec0
anshulhse <anshulhse@gmail.com>
2023-08-19 14:23:54 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:dice_roller/gradient_container.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: GradientContainer(
+          Color.fromARGB(255, 90, 69, 45),
+          Color.fromARGB(255, 27, 240, 255),
+        ),
+      ),
+    ),
+  );
+}
